29def8bc4198744899d35c431c94e61fe600a122
leandroaps <leandroaps@gmail.com>
2023-02-02 17:22:45 -0300

    feat: Adding ESLint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 
 const DummyComponent: React.FC = () => {
   const ref = useRef<HTMLInputElement>(null)
