a01487d8dbc8c6ed4be32f2764d81f052a7f8789
leandroaps <leandroaps@gmail.com>
2023-02-08 19:36:47 -0300

    feat: 🎸 git-cz

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,12 @@
-import { render, screen } from '@testing-library/react'
-import React from 'react'
+import { render, screen } from '@testing-library/react';
+import React from 'react';
 
-import App from './App'
+import App from './App';
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(
+    /Simple React Type and Read with Typescript/i
+  )
   expect(linkElement).toBeInTheDocument()
 })
